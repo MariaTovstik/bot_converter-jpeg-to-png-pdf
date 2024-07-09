1c547d89def31805c4f4f683d745202e0ebf9444
Tovstik Maria <tovstok0608@gmail.com>
2024-07-09 16:53:52 +0300

    png support, edited readme

--- bot_converter.py
+++ bot_converter.py
@@ -3,14 +3,14 @@ import telebot
 from PIL import Image
 from telebot import types
 
-bot = telebot.TeleBot('YOUR API')
+bot = telebot.TeleBot('YOUR API-KEY')
 chats_data = {}
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, f'Вiтаю, {message.from_user.first_name}! Надішліть мені JPEG зображення, щоб'
-                                      f' конвертувати його в PNG і PDF.')
+    bot.send_message(message.chat.id, f'Вiтаю, {message.from_user.first_name}! Надішліть мені JPEG або PNG'
+                                      f' зображення, щоб конвертувати його в PDF, PNG або JPEG.')
 
 
 @bot.message_handler(content_types=['document'])
@@ -29,8 +29,16 @@ def handle_docs_photo(message):
 
             bot.send_message(message.chat.id, "Оберiть формат конвертацiї:", reply_markup=markup)
             chats_data[message.chat.id] = {'img': img}
+        elif message.document.mime_type == 'image/png':
+            img = Image.open(io.BytesIO(downloaded_file))
+            markup = types.InlineKeyboardMarkup()
+            jpeg_button = types.InlineKeyboardButton('JPEG', callback_data='jpeg')
+            pdf_button = types.InlineKeyboardButton("PDF", callback_data="pdf")
+            markup.add(jpeg_button, pdf_button)
+            bot.send_message(message.chat.id, "Оберiть формат конвертацiї:", reply_markup=markup)
+            chats_data[message.chat.id] = {'img': img}
         else:
-            bot.send_message(message.chat.id, 'Будь ласка, надішліть зображення у форматі JPEG.')
+            bot.send_message(message.chat.id, 'Будь ласка, надішліть зображення у форматі JPEG або PNG.')
 
     except Exception as e:
         print(e)
@@ -54,10 +62,18 @@ def handle_callback_query(call):
             pdf_bio.seek(0)
             bot.send_document(call.message.chat.id, pdf_bio, visible_file_name=f'converted_image.pdf')
 
-        # bot.answer_callback_query(call.id, "Виконано!")
+        elif call.data == "jpeg":
+            # Конвертацiя в JPEG
+            jpeg_bio = io.BytesIO()
+            img.save(jpeg_bio, format='JPEG')
+            jpeg_bio.seek(0)
+            bot.send_document(call.message.chat.id, jpeg_bio, visible_file_name='converted_image.jpg')
+
+        bot.send_message(call.message.chat.id, "Виконано!")
+
     except Exception as e:
         print(e)
         bot.send_message(call.message.chat.id, 'Виникла помилка при обробці файлу.')
 
 
-bot.polling(none_stop=True)
+bot.polling(non_stop=True)
